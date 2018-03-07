d60d5febed575dac857a6a778e1b388213d786ce
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-03-07 14:27:11 -0700

    styled employee homepage pending audit approvals

--- app/policies/audit_log_policy.rb
+++ app/policies/audit_log_policy.rb
@@ -1,7 +1,11 @@
 class AuditLogPolicy < ApplicationPolicy
   def index?
+    # TODO Refactor
     return true if admin?
-    
+  end
+
+  def confirm?
+    record.user_id == user.id
   end
 
   private
@@ -9,5 +13,4 @@ class AuditLogPolicy < ApplicationPolicy
     def admin?
       admin_types.include?(user.type)
     end
-
 end
